27e365c83d9a806fac5045cfd146a77cbafb7b49
Poscat <poscat@mail.poscat.moe>
2020-12-25 11:53:24 +0800

    xserver: remove patches

--- modules/xserver/default.nix
+++ modules/xserver/default.nix
@@ -7,17 +7,7 @@
       ./display-manager
       ./desktop-manager
       ./window-manager
-      ../../patches/nixos/modules/services/x11/desktop-managers
-      ../../patches/nixos/modules/services/x11/display-managers
-      ../../patches/nixos/modules/services/x11/window-managers
     ];
-
-  disabledModules = [
-    "services/x11/desktop-managers/default.nix"
-    "services/x11/display-managers/default.nix"
-    "services/x11/window-managers/default.nix"
-    "services/x11/window-managers/none.nix"
-  ];
 
   services.xserver = {
     enable = true;
