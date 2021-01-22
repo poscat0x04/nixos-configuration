949c19e87465cf3b189c29349ce8129d4a4e1869
Poscat <poscat@mail.poscat.moe>
2021-01-22 17:53:48 +0800

    im: switch to fcitx5

--- modules/im.nix
+++ modules/im.nix
@@ -2,9 +2,13 @@
 
 {
   i18n.inputMethod = {
-    enabled = "fcitx";
-    fcitx.engines = with pkgs.fcitx-engines; [
-      rime
+    enabled = "fcitx5";
+    fcitx5.addons = with pkgs; [
+      fcitx5-rime
+      fcitx5-material-color
+      fcitx5-table-extra
+      fcitx5-table-other
+      fcitx5-chinese-addons
     ];
   };
 }
