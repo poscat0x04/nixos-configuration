5eb1df728299d087356037acd2b541e86fc06e07
Poscat <poscat@mail.poscat.moe>
2021-07-07 17:50:16 +0800

    modules/chromium: add environment variables for api key

--- modules/chromium.nix
+++ modules/chromium.nix
@@ -7,12 +7,18 @@
 
   environment = {
     etc."chromium/native-messaging-hosts/org.kde.plasma.browser_integration.json".source = "${pkgs.plasma-browser-integration}/etc/chromium/native-messaging-hosts/org.kde.plasma.browser_integration.json";
+    sessionVariables = {
+      GOOGLE_DEFAULT_CLIENT_ID = "77185425430.apps.googleusercontent.com";
+      GOOGLE_DEFAULT_CLIENT_SECRET = "OTJgUOQcT7lO7GsGZq2G4IlT";
+    };
     systemPackages = [
       (pkgs.chromium.override {
         commandLineArgs = lib.intersperse " " ([
           "--password-store=basic"
           "--ignore-gpu-blacklist"
           "--enable-gpu-rasterization"
+          "--use-gl=desktop"
+          "--enable-accelerated-video-decode"
           "--enable-features=VaapiVideoDecoder"
         ] ++ config.programs.chromium.cliArgs);
       })
