e2abc528964bef19b27b89da29f904c5416201cd
Poscat <poscat@mail.poscat.moe>
2021-07-08 16:10:09 +0800

    thinkfan: set fans, fan level policies and sensors

--- hardware/profiles/thinkpad.nix
+++ hardware/profiles/thinkpad.nix
@@ -7,5 +7,29 @@
   services.thinkfan = {
     enable = true;
     smartSupport = true;
+    fans = [
+      {
+        type = "tpacpi";
+        query = "/proc/acpi/ibm/fan";
+      }
+    ];
+    levels = [
+      [ 0   0   55    ]
+      [ 1   48  60    ]
+      [ 2   50  61    ]
+      [ 3   52  63    ]
+      [ 4   56  65    ]
+      [ 5   59  66    ]
+      [ 7   63  90    ]
+      [ 127 85  32767 ]
+    ];
+    sensors = [
+      {
+        type = "hwmon";
+        query = "/sys/class/hwmon";
+        name = "coretemp";
+        indices = [ 1 2 3 4 5 ];
+      }
+    ];
   };
 }
