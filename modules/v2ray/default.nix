06a539da1a156ecd0f8b45afc048b74bc52fc9ad
Poscat <poscat@mail.poscat.moe>
2021-08-17 10:06:07 +0800

    modules/v2ray: use v2ray-rules-dat

--- modules/v2ray/default.nix
+++ modules/v2ray/default.nix
@@ -1,8 +1,11 @@
-{ secrets, ... }:
+{ secrets, pkgs, ... }:
 
 {
   services.v2ray = {
     enable = true;
     config = import ./cfg.nix secrets.v2ray-servers;
+    package = pkgs.v2ray.override {
+      assetOverrides = pkgs.extra-files.v2ray-rules-dat;
+    };
   };
 }
