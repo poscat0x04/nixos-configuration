1a75dadef0ec24d20b761f0b254ead899905afa9
Poscat <poscat@mail.poscat.moe>
2021-08-24 13:13:39 +0800

    modules/smartdns: listen on tcp ports as well

--- modules/smartdns.nix
+++ modules/smartdns.nix
@@ -14,6 +14,7 @@
       enable = true;
       settings = {
         bind = [ "127.0.0.1:53" "[::1]:53" ];
+        bind-tcp = [ "127.0.0.1:53" "[::1]:53" ];
         speed-check-mode = "ping,tcp:80";
         cache-persist = true;
         log-level = "warn";
