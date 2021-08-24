bf1f341d4443615a282328adb50a55384020787c
Poscat <poscat@mail.poscat.moe>
2021-08-24 20:59:51 +0800

    profiles/server: init

--- profiles/server.nix
+++ profiles/server.nix
@@ -1,6 +1,26 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   imports = [
+    ./default.nix
+  ];
+
+  boot = {
+    consoleLogLevel = 4;
+
+    kernelParams = [
+      # Trust RDRAND
+      "random.trust_cpu=on"
+      # Turn off CPU mitigations
+      "mitigations=off"
+    ];
+  };
+
+  environment.systemPackages = with pkgs; [
+    traceroute
+    iptraf-ng
+    speedtest-cli
+    iftop
+    nethogs
   ];
 }
