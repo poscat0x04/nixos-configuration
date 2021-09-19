b717679d1b51d7d58c7a1bc47fa92698c4424f0c
Poscat <poscat@mail.poscat.moe>
2021-09-19 15:59:23 +0800

    moduels/fonts: remove outdated option

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -1,30 +1,26 @@
 { config, lib, pkgs, ... }:
 
-let
-  dpi = config.nixos.settings.machine.dpi;
-in
-  {
-    fonts = {
-      enableDefaultFonts = lib.mkForce false;
-      fontconfig = {
-        defaultFonts = {
-          sansSerif = lib.mkBefore [ "Roboto" "Noto Sans CJK SC" ];
-          serif = lib.mkBefore [ "Noto Serif" ];
-          monospace = lib.mkBefore [ "Consolas" ];
-          emoji = lib.mkBefore [ "Blobmoji" "Noto Color Emoji" ];
-        };
-        dpi = lib.mkIf (dpi != null) dpi;
+{
+  fonts = {
+    enableDefaultFonts = lib.mkForce false;
+    fontconfig = {
+      defaultFonts = {
+        sansSerif = lib.mkBefore [ "Roboto" "Noto Sans CJK SC" ];
+        serif = lib.mkBefore [ "Noto Serif" ];
+        monospace = lib.mkBefore [ "Consolas" ];
+        emoji = lib.mkBefore [ "Blobmoji" "Noto Color Emoji" ];
       };
-      fonts = with pkgs; [
-        emacs-all-the-icons-fonts
-        noto-fonts
-        noto-fonts-cjk
-        noto-fonts-emoji-blob-bin
-        noto-fonts-extra
-        ttf-ms-win10
-        iosevka
-        sarasa-gothic
-        roboto
-      ];
     };
-  }
+    fonts = with pkgs; [
+      emacs-all-the-icons-fonts
+      noto-fonts
+      noto-fonts-cjk
+      noto-fonts-emoji-blob-bin
+      noto-fonts-extra
+      ttf-ms-win10
+      iosevka
+      sarasa-gothic
+      roboto
+    ];
+  };
+}
