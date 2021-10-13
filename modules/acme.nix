f52fe4987f56580f8a699bc367c367ddd04af45a
Poscat <poscat@mail.poscat.moe>
2021-10-13 18:17:49 +0800

    modules/acme: generate rsa certs as well

--- modules/acme.nix
+++ modules/acme.nix
@@ -4,19 +4,25 @@ let
   credentialsFile = pkgs.writeText "lego-credentials" ''
     CLOUDFLARE_DNS_API_TOKEN=${secrets.ddns-cloudflare-token}
   '';
+  poscat-moe = {
+    inherit credentialsFile;
+    domain = "poscat.moe";
+    dnsProvider = "cloudflare";
+  };
+  poscat-moe-wildcard = {
+    inherit credentialsFile;
+    domain = "*.poscat.moe";
+    dnsProvider = "cloudflare";
+  };
 in {
   security.acme = {
     email = "poscat@mail.poscat.moe";
     acceptTerms = true;
-    certs."poscat.moe" = {
-      inherit credentialsFile;
-      domain = "poscat.moe";
-      dnsProvider = "cloudflare";
-    };
-    certs."poscat.moe-wildcard" = {
-      inherit credentialsFile;
-      domain = "*.poscat.moe";
-      dnsProvider = "cloudflare";
+    certs = {
+      "poscat.moe" = poscat-moe;
+      "poscat.moe-wildcard" = poscat-moe-wildcard;
+      "poscat.moe-rsa" = poscat-moe // { keyType = "rsa4096"; };
+      "poscat.moe-wildcard-rsa" = poscat-moe-wildcard // { keyType = "rsa4096"; };
     };
   };
 }
