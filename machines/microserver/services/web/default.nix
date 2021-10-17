fa937501bdf70e139e12859d6852334d18f90257
Poscat <poscat@mail.poscat.moe>
2021-10-17 11:48:14 +0800

    microserver/web/webdav: init

--- machines/microserver/services/web/default.nix
+++ machines/microserver/services/web/default.nix
@@ -3,6 +3,9 @@
 let
   cfg = config.nixos.settings;
 in {
+  imports = [
+    ./webdav.nix
+  ];
   users = {
     users."${cfg.system.user}".extraGroups = [ "http" ];
     groups = {
---
+++ machines/microserver/services/web/webdav.nix
@@ -0,0 +1,62 @@
+{ pkgs, secrets, ... }:
+
+{
+  services.nginx = {
+    additionalModules = [ pkgs.nginxModules.fancyindex ];
+    virtualHosts."webdav.poscat.moe" = {
+      onlySSL = true;
+      listen = [
+        {
+          addr = "0.0.0.0";
+          port = 8443;
+          ssl = true;
+        }
+      ];
+      useACMEHost = "poscat.moe-wildcard";
+      locations = {
+        "/" = {
+          root = "/srv/http/webdav";
+          extraConfig = ''
+            auth_digest_user_file ${secrets.http-password-digest};
+            auth_digest 'webdav users';
+
+            fancyindex on;
+            fancyindex_localtime on;
+            fancyindex_exact_size off;
+            fancyindex_show_path on;
+            fancyindex_header "/theme/header.html";
+            fancyindex_footer "/theme/footer.html";
+            fancyindex_name_length 255;
+
+            set $dest $http_destination;
+            if (-d $request_filename) {
+              rewrite ^(.*[^/])$ $1/;
+              set $dest $dest/;
+            }
+            if ($request_method ~ (MOVE|COPY)) {
+              more_set_input_headers 'Destination: $dest';
+            }
+
+            if ($request_method ~ MKCOL) {
+              rewrite ^(.*[^/])$ $1/ break;
+            }
+
+            dav_methods PUT DELETE MKCOL COPY MOVE;
+            dav_ext_methods PROPFIND OPTIONS;
+            dav_access user:rw group:rw;
+            create_full_put_path  on;
+
+            client_max_body_size 0;
+          '';
+        };
+        "/theme/" = {
+          alias = "${pkgs.extra-files.nginx-fancyindex-flat-theme}/";
+        };
+      };
+      extraConfig = ''
+        error_page 497 301 =307 https://$host:$server_port$request_uri;
+        add_header Strict-Transport-Security 'max-age=31536000' always;
+      '';
+    };
+  };
+}
