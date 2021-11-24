4834f3aeb802fed957083969ce85d5c26c7aa998
Poscat <poscat@poscat.moe>
2021-11-24 15:26:37 +0800

    emacs: fix snippet directory

--- home-manager/emacs-new/lisp/init-snippet.el
+++ home-manager/emacs-new/lisp/init-snippet.el
@@ -5,7 +5,9 @@
 (use-package yasnippet
   :init (yas-global-mode)
   :bind (:map yas-minor-mode-map
-         ("C-j" . yas-expand)))
+         ("C-j" . yas-expand))
+  :custom
+  (yas-snippet-dirs (cons "~/.config/emacs/snippets" yas-snippet-dirs)))
 
 
 (provide 'init-snippet)
