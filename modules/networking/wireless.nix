a770a5589d8fa00abe4c10dd010c061efbec555d
Poscat <poscat@poscat.moe>
2021-11-24 15:27:39 +0800

    modules/networking/wireless: tune wpa_supplicant

--- modules/networking/wireless.nix
+++ modules/networking/wireless.nix
@@ -5,5 +5,11 @@
     enable = true;
     userControlled.enable = true;
     networks = secrets.wireless-networks;
+    scanOnLowSignal = false;
+    extraConfig = ''
+      bgscan="simple:5:-70:3600"
+      fast_reauth=1
+      ap_scan=1
+    '';
   };
 }
