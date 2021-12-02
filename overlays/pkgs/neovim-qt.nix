44b07ff1ff1de644adb9bafde375bc660b8c66d4
Poscat <poscat@poscat.moe>
2021-12-02 14:33:10 +0800

    override neovim-qt with a development version

---
+++ overlays/pkgs/neovim-qt.nix
@@ -0,0 +1,12 @@
+self: super:
+
+{
+  neovim-qt-unwrapped = super.neovim-qt-unwrapped.overrideAttrs (_: {
+    src = super.fetchFromGitHub {
+      owner = "euqalsraf";
+      repo = "neovim-qt";
+      rev = "e7a51dd58a4a10147d34e93d20b19eeeffc69814";
+      sha256 = "/EbLrJwMd2brclvJWMniwtexY5VURCGUUTjLV/FqE7o=";
+    };
+  });
+}
