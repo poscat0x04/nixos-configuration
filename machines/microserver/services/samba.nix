643035a869a9574a24bc4899334cb52c1b141920
Poscat <poscat@poscat.moe>
2022-03-23 19:18:24 +0800

    microserver/samba: fix permissions on macos

--- machines/microserver/services/samba.nix
+++ machines/microserver/services/samba.nix
@@ -5,19 +5,29 @@
     enable = true;
     package = pkgs.sambaFull;
     extraConfig = ''
+      min protocol = SMB2
       workgroup = WORKGROUP
       server string = smbsrv
       netbios name = smbsrv
 
       inherit owner = unix only
 
-      ;hosts allow = 10.1.11. localhost
-      ;hosts deny = 0.0.0.0/0
+      hosts allow = 10.1.10. 10.1.11. localhost
+      hosts deny = 0.0.0.0/0
 
       deadtime = 1200
       use sendfile = yes
       aio read size = 1
       aio write size = 1
+
+      vfs objects = fruit streams_xattr
+      fruit:metadata = stream
+      fruit:model = MacSamba
+      fruit:veto_appledouble = no
+      fruit:posix_rename = yes
+      fruit:zero_file_id = yes
+      fruit:wipe_intentionally_left_blank_rfork = yes
+      fruit:delete_empty_adfiles = yes
 
       load printers = no
       printing = cups
@@ -27,8 +37,6 @@
       poscat = {
         path = "/shares/poscat";
         "valid users" = "poscat";
-        "force user" = "poscat";
-        "force group" = "users";
         "guest ok" = "no";
         public = "no";
         writable = "yes";
