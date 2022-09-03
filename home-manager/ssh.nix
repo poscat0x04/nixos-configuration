26d3ec672e17a0b2cabe7c5c324c4cf7e3559586
poscat <poscat@poscat.moe>
2022-09-03 13:32:37 +0800

    home-manager/ssh: cleanup

--- home-manager/ssh.nix
+++ home-manager/ssh.nix
@@ -3,31 +3,31 @@
 {
   programs.ssh = {
     enable = true;
+    extraConfig = ''
+      AddKeysToAgent yes
+    '';
     matchBlocks = {
       bwh = {
         hostname = "64.64.228.47";
+        forwardAgent = true;
         forwardX11 = true;
         forwardX11Trusted = true;
       };
       tbwh = {
         hostname = "10.1.11.3";
+        forwardAgent = true;
         forwardX11 = true;
         forwardX11Trusted = true;
-      };
-      archcn = {
-        hostname = "build.archlinuxcn.org";
-        serverAliveInterval = 30;
       };
       router = {
         hostname = "home.poscat.moe";
+        forwardAgent = true;
         forwardX11 = true;
         forwardX11Trusted = true;
       };
-      rpi = {
-        hostname = "192.168.1.187";
-      };
       microserver = {
         hostname = "10.1.10.1";
+        forwardAgent = true;
         forwardX11 = true;
         forwardX11Trusted = true;
       };
