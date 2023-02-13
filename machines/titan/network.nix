b86f564e9a4b338881393fdceea3ed44fe5e4f98
poscat <poscat@poscat.moe>
2023-02-13 21:41:35 +0800

    machines/titan: more network config

--- machines/titan/network.nix
+++ machines/titan/network.nix
@@ -1,7 +1,26 @@
-{ networklib, ... }:
+{ secrets, networklib, nixosModules, ... }:
 
 {
+  imports = [
+    nixosModules.routeupd
+  ];
+
+  networking.forward = true;
+
   services.unbound.additionalInterfaces = [ "10.1.10.1" ];
+
+  networking.pppoe = {
+    enable = true;
+    underlyingIF = "enp2s2";
+    user = secrets.pppoe.china_unicom.user;
+    password = secrets.pppoe.china_unicom.password;
+  };
+
+  services.routeupd = {
+    enable = true;
+    interface = "ppp0";
+    table = 25;
+  };
 
   systemd.network.networks = {
     "11-ignore-wan" = networklib.makeWANConfig {ifname = "enp2s2";};
@@ -13,6 +32,29 @@
 
     "13-upstream" = networklib.makeTrustedDHCPConfig {metric = 20;} // {
       matchConfig.Name = "enp2s4";
+    };
+  };
+
+  networking = {
+    firewall = {
+      trustedInterfaces = [ "ens35" "ens36" ];
+      logRefusedConnections = false;
+    };
+    fwng = {
+      flowtable.devices = [ "ens35" "ens36" ];
+      nat.enable = true;
+      nat66.enable = true;
+      nftables-service = {
+        ppp0-rules = {
+          description = "Set up nftables rules (forwarding, filtering) when ppp0 is created";
+          deviceMode = {
+            enable = true;
+            interface = "ppp0";
+            offload = true;
+            nat.masquerade = true;
+          };
+        };
+      };
     };
   };
 }
