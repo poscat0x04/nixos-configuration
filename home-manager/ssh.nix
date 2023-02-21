3e49b7a486fcac80de52f29d61042cebced383c5
poscat <poscat@poscat.moe>
2023-02-21 21:53:45 +0800

    update ssh hosts

--- home-manager/ssh.nix
+++ home-manager/ssh.nix
@@ -1,6 +1,12 @@
 { secrets,... }:
 
-{
+let
+  mkHost = host: {
+    hostname = host;
+    forwardX11 = true;
+    forwardX11Trusted = true;
+  };
+in{
   programs.ssh = {
     enable = true;
     extraConfig = ''
@@ -8,30 +14,10 @@
     '';
     forwardAgent = true;
     matchBlocks = {
-      bwh = {
-        hostname = "64.64.228.47";
-        forwardAgent = true;
-        forwardX11 = true;
-        forwardX11Trusted = true;
-      };
-      tbwh = {
-        hostname = "10.1.11.3";
-        forwardAgent = true;
-        forwardX11 = true;
-        forwardX11Trusted = true;
-      };
-      router = {
-        hostname = "home.poscat.moe";
-        forwardAgent = true;
-        forwardX11 = true;
-        forwardX11Trusted = true;
-      };
-      microserver = {
-        hostname = "10.1.10.1";
-        forwardAgent = true;
-        forwardX11 = true;
-        forwardX11Trusted = true;
-      };
+      bwh = mkHost "64.64.228.47";
+      titan = mkHost "titan.poscat.moe";
+      hyperion = mkHost "hyperion.poscat.moe";
+      nuc = mkHost "nuc.poscat.moe";
     };
   };
 }
--- modules/ssh.nix
+++ modules/ssh.nix
@@ -1,6 +1,8 @@
 { secrets, ... }:
 
-{
+let
+  makeKnownHosts = s: builtins.mapAttrs (_: v: {publicKey = v;}) s;
+in {
   programs.ssh = {
     setXAuthLocation = true;
     hostKeyAlgorithms = [
@@ -8,36 +10,14 @@
       "ssh-rsa"
     ];
 
-    knownHosts = {
-      github = {
-        extraHostNames = [
-          "github.com"
-        ];
-        publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
-      };
-      gitlab = {
-        extraHostNames = [
-          "gitlab.com"
-        ];
-        publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAfuCHKVTjquxvt6CM6tdG4SLp1Btn/nOeHHE5UOzRdf";
-      };
-      archcn = {
-        extraHostNames = [
-          "build.archlinuxcn.org"
-        ];
-        publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFiYTB+9JVjER580kp4YTgldaAG9NgjbL+EFh9LD1LIt";
-      };
-      bwh = {
-        extraHostNames = [ "64.64.228.47" "10.1.11.3" ];
-        publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM+UBnDMZ76BSYPGj31tFTZtB42413mmExM/Tqrcy7zc";
-      };
-      router = {
-        extraHostNames = [
-          "home.poscat.moe"
-          "10.1.10.1"
-        ];
-        publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOH3IXHQFOETkbtfUJdfgM9M38GnLjU1ssBahgPJv4wv";
-      };
+    knownHosts = makeKnownHosts {
+      "github.com" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
+      "gitlab.com" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAfuCHKVTjquxvt6CM6tdG4SLp1Btn/nOeHHE5UOzRdf";
+      "build.archlinuxcn.org" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFiYTB+9JVjER580kp4YTgldaAG9NgjbL+EFh9LD1LIt";
+      "64.64.228.47" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM+UBnDMZ76BSYPGj31tFTZtB42413mmExM/Tqrcy7zc";
+      "nuc.poscat.moe" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIyIaZR8gh7z3ZKYJJunVfeiFButXiCBp5Mciv2h1Ayy";
+      "titan.poscat.moe" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBFu4YEa65Onqx3o4GeGrqIyc+S2OEPgBQSdBtt1HDAR";
+      "hyperion.poscat.moe" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOxuqPlqGGla89vfFBn9oOMK1trR0mjXE9KeWvVyK7cB";
     };
   };
 }
