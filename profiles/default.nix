6186f89dcf52ba491f81bb225bc961c2741b965d
poscat <poscat@poscat.moe>
2023-02-25 14:19:22 +0800

    profiles/default: add en_DK locale

--- profiles/default.nix
+++ profiles/default.nix
@@ -93,9 +93,15 @@
     defaultLocale = "en_US.UTF-8";
     supportedLocales = [
       "en_US.UTF-8/UTF-8"
+      "en_DK.UTF-8/UTF-8"
       "ja_JP.UTF-8/UTF-8"
       "zh_CN.UTF-8/UTF-8"
     ];
+    extraLocaleSettings = {
+      LC_TIME = "en_DK.UTF-8";
+      LC_PAPER = "en_DK.UTF-8";
+      LC_MEASUREMENT = "en_DK.UTF-8";
+    };
   };
 
   console.keyMap = "us";
