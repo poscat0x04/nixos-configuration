b92d6193dcab0e695eed8c81aba4b8a4d6bb5fbc
poscat <poscat@poscat.moe>
2023-02-28 22:03:24 +0800

    hyperion: centralized journaling

--- machines/hyperion/services/journal.nix
+++ machines/hyperion/services/journal.nix
@@ -1,4 +1,7 @@
+{ pkgs, ... }:
+
 {
+  # journald
   systemd.services.systemd-journal-flush = {
     requires = [ "zfs-mount.service" ];
     after = [ "zfs-mount.service" ];
@@ -11,5 +14,33 @@
       SystemMaxUse=200G
       SystemMaxFileSize=2G
     '';
+  };
+
+  # journal-remote
+  systemd.additionalUpstreamSystemUnits = [
+    "systemd-journal-remote.socket"
+    "systemd-journal-remote.service"
+  ];
+
+  environment.etc."systemd/journal-remote.conf".text = ''
+    [Remote]
+    SplitMode=host
+  '';
+
+  systemd.sockets.systemd-journal-remote = {
+    wantedBy = [ "sockets.target" ];
+  };
+
+  users.users.systemd-journal-remote = {
+    description = "journal remote sink user";
+    isSystemUser = true;
+    group = "systemd-journal";
+  };
+
+  systemd.services.systemd-journal-remote = {
+    serviceConfig = {
+      ExecStart = [ "" "${pkgs.systemd}/lib/systemd/systemd-journal-remote --listen-http=-3 --output=/var/log/journal/remote/" ];
+      Group = "systemd-journal";
+    };
   };
 }
