58d0e5ffb6d7fec5d860f39292247711b6e5f337
poscat <poscat@poscat.moe>
2023-03-01 06:45:39 +0800

    nuc: upload journal to hyperion

--- machines/nuc/default.nix
+++ machines/nuc/default.nix
@@ -1,4 +1,4 @@
-{...}:
+{ lib, ... }:
 
 {
   imports = [
@@ -7,6 +7,7 @@
     ../../hardware/profiles/uefi.nix
     ../../hardware/cpu/intel
     ../../modules/networking/warp.nix
+    ../../modules/journal-upload.nix
     ./router/dns.nix
     ./router/firewall.nix
     ./router/networks.nix
@@ -23,6 +24,13 @@
   };
 
   services.timesyncd.enable = false;
+
+  systemd.services.systemd-journal-upload = {
+    wantedBy = lib.mkForce [ "sys-subsystem-net-devices-ppp0.device" ];
+    requires = [ "sys-subsystem-net-devices-ppp0.device" ];
+    after = [ "sys-subsystem-net-devices-ppp0.device" ];
+    serviceConfig.ExecStartPre = "/run/current-system/sw/bin/sleep 5";
+  };
 
   system.stateVersion = "22.11";
 }
