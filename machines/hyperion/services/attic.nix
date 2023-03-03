01a1afb0e398ba2ec9c4fe08b765ef67ef9ab288
poscat <poscat@poscat.moe>
2023-03-03 14:15:46 +0800

    hyperion/attic: separate API and binary cache

--- machines/hyperion/services/attic.nix
+++ machines/hyperion/services/attic.nix
@@ -11,8 +11,9 @@
       credentialsFile = config.sops.secrets.attic-secret.path;
       settings = {
         listen = "127.0.0.1:20843";
-        allowed-hosts = [ "cache.poscat.moe" ];
-        api-endpoint = "https://cache.poscat.moe:8443/";
+        allowed-hosts = [ "cache.poscat.moe" "attic.poscat.moe" ];
+        api-endpoint = "https://attic.poscat.moe:8443/";
+        substituter-endpoint = "https://cache.poscat.moe:8443/";
         database.url = "postgresql://atticd@%2frun%2fpostgresql/attic";
         chunking = {
           nar-size-threshold = 64 * 1024;
@@ -28,26 +29,49 @@
       };
     };
 
-    nginx.virtualHosts."cache.poscat.moe" = {
-      onlySSL = true;
-      useACMEHost = "poscat.moe-wildcard";
-      kTLS = true;
-      listen = [
-        {
-          addr = "0.0.0.0";
-          port = 8443;
-          ssl = true;
-        }
-      ];
-      locations = {
-        "/" = {
-          proxyPass = "http://127.0.0.1:20843";
-          extraConfig = ''
-            # disable upload size limit
-            client_max_body_size 0;
-            proxy_set_header X-Real-IP $remote_addr;
-            proxy_set_header Host $host;
-          '';
+    nginx.virtualHosts = {
+      "attic.poscat.moe" = {
+        onlySSL = true;
+        useACMEHost = "poscat.moe-wildcard";
+        kTLS = true;
+        listen = [
+          {
+            addr = "0.0.0.0";
+            port = 8443;
+            ssl = true;
+          }
+        ];
+        locations = {
+          "/" = {
+            proxyPass = "http://127.0.0.1:20843";
+            extraConfig = ''
+              # disable upload size limit
+              client_max_body_size 0;
+              proxy_set_header X-Real-IP $remote_addr;
+              proxy_set_header Host $host;
+            '';
+          };
+        };
+      };
+      "cache.poscat.moe" = {
+        onlySSL = true;
+        useACMEHost = "poscat.moe-wildcard";
+        kTLS = true;
+        listen = [
+          {
+            addr = "0.0.0.0";
+            port = 8443;
+            ssl = true;
+          }
+        ];
+        locations = {
+          "/" = {
+            proxyPass = "http://127.0.0.1:20843";
+            extraConfig = ''
+              proxy_set_header X-Real-IP $remote_addr;
+              proxy_set_header Host $host;
+            '';
+          };
         };
       };
     };
