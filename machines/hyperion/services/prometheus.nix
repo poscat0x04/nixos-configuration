0768d81111c6c8d652301a0e99336ae3838a56e9
poscat <poscat@poscat.moe>
2023-10-07 13:47:36 +0800

    hyperion/prometheus: scrape more machines

--- machines/hyperion/services/prometheus.nix
+++ machines/hyperion/services/prometheus.nix
@@ -4,6 +4,11 @@
   services.prometheus = {
     enable = true;
     stateDir = "prometheus";
+    enableReload = true;
+    globalConfig = {
+      scrape_interval = "30s";
+      evaluation_interval = "30s";
+    };
     scrapeConfigs = [
       {
         job_name = "hyperion";
@@ -13,6 +18,35 @@
               "127.0.0.1:${toString config.services.prometheus.exporters.node.port}"
               "127.0.0.1:${toString config.services.prometheus.exporters.smartctl.port}"
             ];
+            labels = {
+              hostname = "hyperion";
+            };
+          }
+        ];
+      }
+      {
+        job_name = "nuc";
+        static_configs = [
+          {
+            targets = [
+              "10.1.20.1:${toString config.services.prometheus.exporters.node.port}"
+            ];
+            labels = {
+              hostname = "nuc";
+            };
+          }
+        ];
+      }
+      {
+        job_name = "dione";
+        static_configs = [
+          {
+            targets = [
+              "10.1.10.4:9100"
+            ];
+            labels = {
+              hostname = "nuc";
+            };
           }
         ];
       }
