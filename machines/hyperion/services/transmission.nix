5c96828a32a777e5dfe2edb1c405e7349e90d23a
poscat <poscat@poscat.moe>
2023-11-13 11:17:07 +0800

    hyperion/transmission: use openidc auth

--- machines/hyperion/services/transmission.nix
+++ machines/hyperion/services/transmission.nix
@@ -21,7 +21,64 @@
 
         rpc-host-whitelist-enabled = false;
         rpc-whitelist-enabled = false;
-        rpc-bind-address = "10.1.10.3";
+        rpc-bind-address = "127.0.0.1";
+      };
+    };
+
+    nginx = {
+      virtualHosts."transmission.poscat.moe" = {
+        onlySSL = true;
+        useACMEHost = "poscat.moe-wildcard";
+        kTLS = true;
+        listen = [
+          {
+            addr = "0.0.0.0";
+            port = 8443;
+            ssl = true;
+          }
+          {
+            addr = "[::0]";
+            port = 8443;
+            ssl = true;
+          }
+        ];
+        locations."/" = {
+          proxyPass = "http://localhost:9091";
+          recommendedProxySettings = true;
+          extraConfig = ''
+            access_by_lua_block {
+              local opts = {
+                redirect_uri_path = "/callback",
+                discovery = "https://git.poscat.moe:8443/.well-known/openid-configuration",
+                client_id = "c7352e25-4e47-4240-b8d1-d3518db43762",
+                client_secret = secret.transmission_client_secret,
+                ssl_verify = "yes",
+                scope = "openid email profile groups",
+                redirect_uri_scheme = "https",
+              }
+
+              local res, err = require("resty.openidc").authenticate(opts)
+
+              if err then
+                ngx.status = 500
+                ngx.say(err)
+                ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
+              end
+
+              if res.user.preferred_username ~= "poscat" then
+                ngx.status = 403
+                ngx.exit(ngx.HTTP_FORBIDDEN)
+                ngx.say('Access Denied')
+              end
+
+              ngx.req.set_header("X-USER", res.id_token.sub)
+            }
+          '';
+        };
+        extraConfig = ''
+          error_page 497 301 =307 https://$host:$server_port$request_uri;
+          add_header Strict-Transport-Security 'max-age=31536000' always;
+        '';
       };
     };
   };
