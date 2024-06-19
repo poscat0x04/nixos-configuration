5c5ec70e48bb8beb205fc1eb8e95f3d8ba219c16
poscat <poscat@poscat.moe>
2024-06-19 17:26:27 +0800

    hyperion: update sing-box config

--- machines/hyperion/network.nix
+++ machines/hyperion/network.nix
@@ -102,10 +102,5 @@
   };
   systemd.services.cloudflare-ddns.serviceConfig.Slice = "system-noproxy.slice";
 
-  # WARP
-  networking.warp = {
-    manualActivation = true;
-    v6addr = "2606:4700:110:889f:69da:797a:1461:a409";
-  };
-  networking.fwng.warpId = "0x5440ec";
+  services.sing-box.reserved = [ 190 145 171 ];
 }
